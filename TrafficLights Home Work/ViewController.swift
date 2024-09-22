2918f08326e9271b9edb09a1622d688aa5ca47d0
Artsiom <artsiom.hurynovich@gmail.com>
2024-09-22 12:38:19 +0200

    Initial Commit

---
+++ TrafficLights Home Work/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLights Home Work
+//
+//  Created by Artem H on 22.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
